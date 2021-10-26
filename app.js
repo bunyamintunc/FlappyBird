ec7fe0037d7f47d90a3fb3966f3affa4bf111ea4
Bünyamin Tünç <73202361+bunyaminTunc@users.noreply.github.com>
2021-10-26 12:38:55 +0300

    Oyun kurallari eklendi.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded',()=>{
     const bird=document.querySelector('.bird');
     const gameDisplay= document.querySelector('.game-container');
     const ground=document.querySelector('.ground');
+    let span=document.getElementById('skor'); 
+    let mesageGameOverSpan=document.getElementById('gameOverMesage');
+    let  mesage=document.getElementById('mesage');
+    let skor=0;
 
     let birdLeft=220;
     let birdBottom=100;
@@ -31,34 +35,64 @@ document.addEventListener('DOMContentLoaded',()=>{
     document.addEventListener('keyup',control)
 
     function elemanUret(){
-        let elemanLeft=500
-        let randomHeight=Math.random()*60
-        let elemanBottom=randomHeight
+        span.innerHTML=skor;   
+        let obstacleLeft=500;
+        let randomHeight=Math.random()*60;
+        let obstacleBottom=randomHeight;
+        let gap = 430;
 
-        const eleman=document.createElement('div');
-        eleman.classList.add('obstacle')
-        gameDisplay.appendChild(eleman);
-        eleman.style.left=elemanLeft + 'px'
-        eleman.style.bottom=elemanBottom + 'px'
+        const obstacle=document.createElement('div');
+        const topObstacle=document.createElement('div');
+
+        if(!isGameOver){
+            obstacle.classList.add('obstacle');
+            topObstacle.classList.add('topObstacle');
+        }
+       
+        
+        gameDisplay.appendChild(obstacle);
+        gameDisplay.appendChild(topObstacle)
+
+        obstacle.style.left=obstacleLeft + 'px';
+        topObstacle.style.left=obstacleLeft + 'px';
+
+        obstacle.style.bottom=obstacleBottom + 'px';
+        topObstacle.style.bottom=obstacleBottom+gap+'px';
 
         function moveEleman(){
-            elemanLeft -=2
-            eleman.style.left=elemanLeft +'px'
+            
 
-            if(elemanLeft===-60){
-                clearInterval(timeId)
-                gameDisplay.removeChild(eleman)
+            //oyunu hızlandırıyoruz.
+            if(skor>4){
+                obstacleLeft-=3;
             }
-            if(elemanLeft>200&&elemanLeft<280 &&birdLeft==220&&
-                birdBottom < elemanBottom+153 ||birdBottom===0){
+            else{
+                obstacleLeft -=2;
+            }
+            
+            obstacle.style.left=obstacleLeft +'px';
+            topObstacle.style.left=obstacleLeft + 'px';
+
+            if(obstacleLeft===-60){
+                clearInterval(timeId)
+                gameDisplay.removeChild(obstacle)
+                gameDisplay.removeChild(topObstacle)
+            }
+            if(obstacleLeft>200&&obstacleLeft<280 &&birdLeft==220&&
+                (birdBottom < obstacleBottom+153||birdBottom>obstacleBottom+gap-200 )||birdBottom===0){
                   gameOver()
                   clearInterval(timeId)
             }
+            
+            
         }
         let timeId=setInterval(moveEleman,20)
         if(!isGameOver){
             setTimeout(elemanUret,3000)
         }
+
+        //her uretilen engelde skoru bir arttıryoruz.
+        skor +=1;
 
     }
     elemanUret()
@@ -66,6 +100,9 @@ document.addEventListener('DOMContentLoaded',()=>{
     function gameOver(){
         clearInterval(gameTimerId);
         isGameOver=true;
+        skor=0;
+        mesageGameOverSpan.innerHTML="Game Over !";
+         mesage.classList.add('mesaj')
         document.removeEventListener('keyup',control)
 
     }
